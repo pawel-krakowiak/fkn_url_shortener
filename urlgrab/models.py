f825778c72d043dfebb9cd68ac3251488d523073
FuRaJ3003 <furajchirashi3003@gmail.com>
2022-07-08 02:58:51 +0200

    Added __str__ parse into model

--- urlgrab/models.py
+++ urlgrab/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from urllib.parse import urlparse
 
-# Create your models here.
 class URLGrab(models.Model):
     shorted_url = models.URLField(default="http://127.0.0.8:8000/shorter")
     direction_url = models.URLField(max_length=5000, blank=False)
     stored_time = models.DateTimeField(auto_now=True, editable=False)
+
+    def __str__(self):
+        net_location = urlparse(self.shorted_url).netloc
+        return f"[{self.id}]: {net_location}"
