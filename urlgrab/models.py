14d888d730f8b1fbb451b1135b3c0b44f80f3158
FuRaJ3003 <furajchirashi3003@gmail.com>
2022-07-08 02:48:11 +0200

    Created url grab model

--- urlgrab/models.py
+++ urlgrab/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class URLGrab(models.Model):
+    shorted_url = models.URLField(default="http://127.0.0.8:8000/shorter")
+    direction_url = models.URLField(max_length=5000, blank=False)
+    stored_time = models.DateTimeField(auto_now=True, editable=False)
