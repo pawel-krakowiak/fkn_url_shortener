f56df54f86746ebe0ebcb289d06dbba930952ca4
FuRaJ3003 <furajchirashi3003@gmail.com>
2022-07-12 21:13:29 +0200

    Added net location display and changed field names

--- urlgrab/models.py
+++ urlgrab/models.py
@@ -2,10 +2,11 @@ from django.db import models
 from urllib.parse import urlparse
 
 class URLGrab(models.Model):
-    shorted_url = models.URLField(default="http://127.0.0.8:8000/shorter")
-    direction_url = models.URLField(max_length=5000, blank=False)
+    shorted_url = models.CharField(max_length=300, default="http://127.0.0.8:8000/shorter")
+    link = models.CharField(max_length=10000)
+    uuid = models.CharField(max_length=10)
     stored_time = models.DateTimeField(auto_now=True, editable=False)
 
     def __str__(self):
-        net_location = urlparse(self.direction_url).netloc
+        net_location = urlparse(self.link).netloc
         return f"[{self.id}]: {net_location}"
